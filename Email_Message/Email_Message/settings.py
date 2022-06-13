e3d539aece3f2c657e6c6fff67f8df341ff285fd
behan05 <vihan095678@gmail.com>
2022-06-13 16:14:30 +0530

    fix bug

--- Email_Message/Email_Message/settings.py
+++ Email_Message/Email_Message/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yb6%qb_vt%^6$9*tt9uu58u7klpnoh0bd#x*9=r+dm3e@7vj!3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
